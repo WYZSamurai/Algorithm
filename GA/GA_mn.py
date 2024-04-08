8b851f8bdbaca3b85af870499f40ec83b2ae961e
Samurai <963513255@qq.com>
2024-04-08 13:36:55 +0800

    完成m*n维度的遗传算法

--- GA/GA_mn.py
+++ GA/GA_mn.py
@@ -2,8 +2,19 @@ import plotly.graph_objects as go
 import torch
 
 
+# 选择设备
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+# else:
+#     device = torch.device("cpu")
+
+
+device = torch.device("cpu")
+cpu = torch.device("cpu")
+
+
 def func(x: torch.Tensor):
-    y = x.t()@x
+    y = torch.matmul(x.t(), x)
     return y
 
 
@@ -26,26 +37,93 @@ def plot():
 
 
 def decode(dna: torch.Tensor, mx: float, md: float):
-    pass
+    NP = dna.shape[0]
+    L = dna.shape[3]
+
+    scale = float((md-mx)/(pow(2, L)-1))
+    a = (2*torch.ones(L,)).pow(torch.arange(0, L)
+                               ).to(dtype=torch.float, device=device)
+    a, _ = a.sort(descending=True)
+    x = scale*torch.matmul(dna, a)+mx
+
+    # 计算适应值
+    fit = torch.zeros(NP,).to(dtype=torch.float, device=device)
+    for i in range(NP):
+        fit[i] = func(x[i])
+
+    # 筛出适应度最值
+    maxindex = torch.argmax(fit)
+    minindex = torch.argmin(fit)
+
+    # 保存最佳个体的dna、十进制值、适应度
+    dnabest = dna[maxindex]
+    xbest = x[maxindex]
+    ybest = func(xbest)
+
+    # 适应值归一化操作
+    # 注意dna全部一致情况
+    if fit[maxindex]-fit[minindex] == 0:
+        fit = fit/fit.sum()
+    else:
+        fit = (fit-fit[minindex])/(fit[maxindex]-fit[minindex])
+
+    # print("dna值：\n", dna)
+    # print("X值：\n", x)
+    # print("fit值：\n", fit)
+    print("最佳x值：\n", xbest)
+    # print("最佳y值：\n", ybest)
+    return fit, xbest, ybest, dnabest
 
 
 def selection(dna: torch.Tensor, fit: torch.Tensor):
-    pass
+    NP = dna.shape[0]
+    P = (fit/fit.sum()).reshape(NP,)
+    index = torch.multinomial(input=P, num_samples=NP, replacement=True)
+    dna = dna[index]
+    return dna
 
 
 def crossover(dna: torch.Tensor, Pc: float):
-    pass
+    NP = dna.shape[0]
+    L = dna.shape[3]
+    for i in range(NP, 2):
+        P = torch.rand(size=(1,)).item()
+        if P < Pc:
+            randcut = torch.randint(int(L/2), L, (1,)).item()
+            temp = dna[i, :, :, randcut:].clone()
+            dna[i, :, :, randcut:] = dna[i+1, :, :, randcut:]
+            dna[i+1, :, :, randcut:] = temp
+    return dna
 
 
 def mutation(dna: torch.Tensor, Pm: float):
-    pass
+    P = (torch.rand(size=dna.shape)-Pm * torch.ones(size=dna.shape))
+    dna = torch.where(P > 0, input=dna, other=1-dna)
+    return dna
 
 
-def GA(NP=50, L=20, G=100, Pc=0.8, Pm=0.05, mx=0, md=10):
-    pass
+def GA(NP=50, L=20, m=2, n=1, G=100, Pc=0.8, Pm=0.05, mx=0, md=10):
+    xbest = torch.zeros(size=(G, m, n))
+    ybest = torch.zeros(size=(G,))
+
+    dna = torch.randint(0, 2, (NP, m, n, L)).to(
+        dtype=torch.float, device=device)
+
+    for i in range(G):
+        print("第", i+1, "代")
+        fit, xbest[i], ybest[i], dnabest = decode(dna, mx, md)
+        dna = selection(dna, fit)
+        dna = crossover(dna, Pc)
+        dna = mutation(dna, Pm)
+        dna[0] = dnabest
+
+    bestindex = torch.argmax(ybest)
+    print("最佳x值为：\n", xbest[bestindex])
+    print("最佳函数值为：\n", ybest[bestindex])
+
+    return ybest
 
 
 if __name__ == "__main__":
-    plot()
-
-    GA(NP=50, L=20, G=100, Pc=0.8, Pm=0.05, mx=0, md=10)
+    GA()
+    # plot()
